dbc5fb0f20f17c756b88d36bb69fe2a5fb0d7f5f
hanan_Shihab <HananShihab2001@gmail.com>
2022-09-11 20:06:36 +0300

    Hanan Commit

---
+++ src/b9a_group4_lab01/B9A_Group4_Lab01.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package b9a_group4_lab01;
+
+/**
+ *
+ * @author Hanan
+ */
+public class B9A_Group4_Lab01 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+        System.out.println("Hi ther!! am Hanan Shihab :) ");
+    }
+    
+}
